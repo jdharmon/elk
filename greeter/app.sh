96c6430e166918b15061e1abb137eeca919a38c3
Jason Harmon <jdharmon@sentara.com>
2017-06-01 14:52:46 +0000

    Added sample log message generator.

---
+++ greeter/app.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+while true; do
+  hour=$(date +%H)
+  if [ "$hour" -lt "12" ]; then
+    message="Good morning!"
+  elif [ "$hour" -lt "17" ]; then
+    message="Good afternoon!"
+  else
+    message="Good evening!"
+  fi
+  
+  echo "It is $(date). $message"
+  sleep $(( $RANDOM % 60 ))
+done

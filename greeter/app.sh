61db12998fff2843d13419a1ee9a10b4401b181c
Jason Harmon <jdharmon@sentara.com>
2017-06-02 21:49:03 +0000

    Enabled Elasticsearch clustering on swarm.

--- greeter/app.sh
+++ greeter/app.sh
@@ -3,13 +3,14 @@
 while true; do
   hour=$(date +%H)
   if [ "$hour" -lt "12" ]; then
-    message="Good morning!"
+    message="Good morning"
   elif [ "$hour" -lt "17" ]; then
-    message="Good afternoon!"
+    message="Good afternoon"
   else
-    message="Good evening!"
+    message="Good evening"
   fi
   
-  echo "It is $(date). $message"
+  echo "It is $(date). $message, from $(hostname)!"
   sleep $(( $RANDOM % 60 ))
 done
+
